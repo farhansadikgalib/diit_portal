31b7b9f548dfbde64501ae134c747d7c76ab4603
farhan <farhansadikgalib@gmail.com>
2022-06-24 13:04:59 +0600

    QuestionBank is connected with firebase Firestore.
    Signed-off-by: farhan <farhansadikgalib@gmail.com>

--- lib/Screens/Dashboard/Home_Page/Question_bank/dept_mba/mba_question.dart
+++ lib/Screens/Dashboard/Home_Page/Question_bank/dept_mba/mba_question.dart
@@ -1,230 +1,149 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:diit_portal/Screens/App_Bar/app_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'package:getwidget/getwidget.dart';
 
 class MBADepertment extends StatelessWidget {
-  final List semestername = [
-    "1st Semester",
-    "2nd Semester",
-    "3rd Semester",
+  final Stream<QuerySnapshot> _QBStream = FirebaseFirestore.instance.collection('QuestionBank').snapshots();
 
-  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarString,
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 30,left: 15),
-                  child: Text(
-                    "Depertment",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'Baloo',
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(height: 80,),
-                SizedBox(
+    return  SafeArea(
+      child: Scaffold(
+        body: StreamBuilder<QuerySnapshot>(
+          stream: _QBStream,
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
 
-                  width: 200.0,
-                  child: DefaultTextStyle(
-                    style: const TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Baloo',
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator(color: Colors.orange,));
+            }
+            if(snapshot.connectionState==ConnectionState.active){
+              QuerySnapshot? querySnapshot = snapshot.data;
+            }
 
-                    ),
-                    child: AnimatedTextKit(
-                      animatedTexts: [
-                        RotateAnimatedText('MASTER'),
-                        RotateAnimatedText('BUSINESS'),
-                        RotateAnimatedText('ADMINISTRATION'),
-
-                      ],
-                      onTap: () {
-                        print("Tap Event");
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 10,right: 20,left: 20),
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: ListView.builder(
-                    itemCount: semestername.length,
-                    itemBuilder: (BuildContext context,int index){
-                      return Card(
-                        elevation: 3,
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          height: 70,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            border: Border(
-                              top: BorderSide(color: Colors.orange, width: 4),
-                              left: BorderSide(color: Colors.orange, width: 4),
-                              right: BorderSide(color: Colors.orange, width: 4),
-                              bottom: BorderSide(color: Colors.orange, width: 4),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 10, right: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "${semestername[index]}",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 19,
-                                      fontFamily: 'Baloo'),
-                                ),
-                                GFButton(
-                                  size: 50,
-                                  color: Colors.green,
-                                  onPressed: () =>Get.toNamed('/pdf_viewPage'),
-                                  text: "Download",
-                                  textStyle: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                  position: GFPosition.end,
-                                  icon: Icon(
-                                    Icons.arrow_downward_outlined,
-                                    color: Colors.white,
-                                  ),
-                                  type: GFButtonType.solid,
-                                  shape: GFButtonShape.standard,
-                                ),
-                              ],
-                            ),
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                return Column(
+                  children: [
+                    SizedBox(height: 5,),
+                    Card(
+                      elevation: 3,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        height: 75,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(color: Colors.orange, width: 4),
+                            left: BorderSide(color: Colors.orange, width: 4),
+                            right: BorderSide(color: Colors.orange, width: 4),
+                            bottom: BorderSide(color: Colors.orange, width: 4),
                           ),
                         ),
-                      );
-                    }
-                ),
-              ),
-            ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                // "${data['1st Semester']}"
+                                '1st Semester' ,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    fontFamily: 'Baloo'),
+                              ),
+                              GFButton(
+                                size: 50,
+                                color: Colors.green,
+                                onPressed: () {
+                                  var first= data['1st Semester'];
+                                  print(first);
+                                  Get.toNamed('/pdf_viewPage',arguments: [data['1st Semester']]);
+                                },
+                                text: "View",
+                                textStyle: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    fontFamily: 'Baloo'),
+                                position: GFPosition.end,
+                                icon: Icon(
+                                  Icons.arrow_downward_outlined,
+                                  size: 22,
+                                  color: Colors.white,
+                                ),
+                                type: GFButtonType.solid,
+                                shape: GFButtonShape.standard,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 3,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        height: 75,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(color: Colors.orange, width: 4),
+                            left: BorderSide(color: Colors.orange, width: 4),
+                            right: BorderSide(color: Colors.orange, width: 4),
+                            bottom: BorderSide(color: Colors.orange, width: 4),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '2nd Semester' ,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    fontFamily: 'Baloo'),
+                              ),
+                              GFButton(
+                                size: 50,
+                                color: Colors.green,
+                                onPressed: () {
+                                  var first= data['2nd Semester'];
+                                  print(first);
+                                  Get.toNamed('/pdf_viewPage',arguments: [first]);
+                                },
+                                text: "View",
+                                textStyle: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    fontFamily: 'Baloo'),
+                                position: GFPosition.end,
+                                icon: Icon(
+                                  Icons.arrow_downward_outlined,
+                                  size: 22,
+                                  color: Colors.white,
+                                ),
+                                type: GFButtonType.solid,
+                                shape: GFButtonShape.standard,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
 
-
-            // Card(
-            //   elevation: 3,
-            //   child: Container(
-            //     alignment: Alignment.centerLeft,
-            //     height: 70,
-            //     width: double.infinity,
-            //     decoration: BoxDecoration(
-            //       border: Border(
-            //         top: BorderSide(color: Colors.orange, width: 4),
-            //         left: BorderSide(color: Colors.orange, width: 4),
-            //         right: BorderSide(color: Colors.orange, width: 4),
-            //         bottom: BorderSide(color: Colors.orange, width: 4),
-            //       ),
-            //     ),
-            //     child: Padding(
-            //       padding: const EdgeInsets.only(left: 10, right: 10),
-            //       child: Row(
-            //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //         children: [
-            //           Text(
-            //             "1st Semester",
-            //             style: TextStyle(
-            //                 fontWeight: FontWeight.w500,
-            //                 fontSize: 19,
-            //                 fontFamily: 'Baloo'),
-            //           ),
-            //           GFButton(
-            //             size: 50,
-            //             color: Colors.green,
-            //             onPressed: () {
-            //               print('cse 1st');
-            //             },
-            //             text: "Download",
-            //             textStyle: TextStyle(
-            //               fontSize: 20,
-            //             ),
-            //             position: GFPosition.end,
-            //             icon: Icon(
-            //               Icons.arrow_downward_outlined,
-            //               color: Colors.white,
-            //             ),
-            //             type: GFButtonType.solid,
-            //             shape: GFButtonShape.standard,
-            //           ),
-            //         ],
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // Card(
-            //   elevation: 3,
-            //   child: Container(
-            //     alignment: Alignment.centerLeft,
-            //     height: 70,
-            //     width: double.infinity,
-            //     decoration: BoxDecoration(
-            //       border: Border(
-            //         top: BorderSide(color: Colors.orange, width: 4),
-            //         left: BorderSide(color: Colors.orange, width: 4),
-            //         right: BorderSide(color: Colors.orange, width: 4),
-            //         bottom: BorderSide(color: Colors.orange, width: 4),
-            //       ),
-            //     ),
-            //     child: Padding(
-            //       padding: const EdgeInsets.only(left: 10, right: 10),
-            //       child: Row(
-            //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //         children: [
-            //           Text(
-            //             "2nd Semester",
-            //             style: TextStyle(
-            //                 fontWeight: FontWeight.w500,
-            //                 fontSize: 19,
-            //                 fontFamily: 'Baloo'),
-            //           ),
-            //           GFButton(
-            //             size: 50,
-            //             color: Colors.green,
-            //             onPressed: () {
-            //               print('cse 1st');
-            //             },
-            //             text: "Download",
-            //             textStyle: TextStyle(
-            //               fontSize: 20,
-            //             ),
-            //             position: GFPosition.end,
-            //             icon: Icon(
-            //               Icons.arrow_downward_outlined,
-            //               color: Colors.white,
-            //             ),
-            //             type: GFButtonType.solid,
-            //             shape: GFButtonShape.standard,
-            //           ),
-            //         ],
-            //       ),
-            //     ),
-            //   ),
-            // ),
-           
-          ],
+                );
+              }).toList(),
+            );
+          },
         ),
       ),
     );
--- lib/Screens/Dashboard/Home_Page/Question_bank/pdf_view.dart
+++ lib/Screens/Dashboard/Home_Page/Question_bank/pdf_view.dart
@@ -13,43 +13,28 @@ class pdf_viewPage extends StatefulWidget {
 class _pdf_viewPageState extends State<pdf_viewPage> {
 
 
+  final receivePDF = Get.arguments[0];
 
   RxBool _loaded = false.obs;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          // title: const Text('Syncfusion Flutter PDF Viewer'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(
-                Icons.bookmark,
-                color: Colors.white,
-                semanticLabel: 'Bookmark',
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+          body: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: SfPdfViewer.network(
+                  '${receivePDF.toString()}',
+                  onDocumentLoaded: (PdfDocumentLoadedDetails details) {
+                    _loaded.value = true;
+                  },
+                ),
               ),
-              onPressed: () {
-                // _pdfViewerKey.currentState?.openBookmarkView();
-              },
-            ),
-          ],
-        ),
-        // body: SfPdfViewer.network(
-        //   'https://firebasestorage.googleapis.com/v0/b/diit-portal.appspot.com/o/QuestionBank%2F1stsemester.pdf?alt=media&token=b26875f5-56fa-47af-b46e-c8c067f83bb2',
-        //   key: _pdfViewerKey,
-
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: SfPdfViewer.network(
-                'https://firebasestorage.googleapis.com/v0/b/diit-portal.appspot.com/o/QuestionBank%2F2ndsemester.pdf?alt=media&token=444c9313-bcab-4128-8b42-10998070a7ea',
-                onDocumentLoaded: (PdfDocumentLoadedDetails details) {
-                  _loaded.value = true;
-                },
-              ),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
