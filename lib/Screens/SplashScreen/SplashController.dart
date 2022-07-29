7f415d639f8f5b301172bff189473ebb9464689b
farhan <farhansadikgalib@gmail.com>
2022-07-29 22:58:10 +0600

    trying to set no internet on splashscreen
    Signed-off-by: farhan <farhansadikgalib@gmail.com>

--- lib/Screens/SplashScreen/SplashController.dart
+++ lib/Screens/SplashScreen/SplashController.dart
@@ -1,20 +1,47 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
+import 'package:diit_portal/Screens/Dashboard/Home_Page/NU_Portal/widgets/no_internet.dart';
+import 'package:diit_portal/Screens/Dashboard/Home_Page/NU_Portal/widgets/no_internet_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:get/get.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class SplashController extends GetxController {
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
     moveToNext();
+    ConnectivityCheck();
+
+  }
+
+  ConnectivityCheck(){
+    NoInternet.initConnectivity().then((value) =>
+      _connectionStatus = value);
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+          NoInternet.updateConnectionStatus(result).then((value) =>
+            _connectionStatus = value);
+        });
   }
 
   Future<void> moveToNext() async {
     await Future.delayed(const Duration(milliseconds: 4000), () {});
     // FirebaseDatabase.instance.setPersistenceEnabled(true);
-    Get.offAndToNamed('/LoginPage');
+    if(_connectionStatus == 'ConnectivityResult.none'){
+      NoInternetWidget();
+    }else{
+      Get.offAndToNamed('/LoginPage');
+
+    }
+
   }
 
 
